fc5062426f17d1dbd1f01070566116ae59759b6e
memmsc <xiongfei>
2021-07-27 15:07:21 +0800

    feat：优化代码

--- src/views/wemirr/management/user/index.vue
+++ src/views/wemirr/management/user/index.vue
@@ -1,5 +1,12 @@
 <template>
   <fs-crud ref="crudRef" v-bind="crudBinding">
+    <template #actionbar-right>
+      <a-alert
+        class="ml-1"
+        type="info"
+        message="演示环境图片上传不显示,正常Fork代码更改七牛/腾讯云存储/阿里云存储配置即可"
+      />
+    </template>
     <template #cell_nickName="scope">
       <a-tooltip placement="top" :title="scope.row.nickName">
         {{ scope.row.nickName }}
