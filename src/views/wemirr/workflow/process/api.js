be2ef35e9b7dc914487f57cf96e2ec3b6c0be307
唐亚峰 <battcn@cortp.com>
2023-09-23 15:21:21 +0800

    feat：优化代码

--- src/views/wemirr/workflow/process/api.js
+++
@@ -1,9 +0,0 @@
-import { defHttp } from "@/utils/http/axios";
-
-export function BatchDelete(ids) {
-  return defHttp.request({
-    url: `/authority/station_messages`,
-    method: "delete",
-    data: ids,
-  });
-}
--- src/views/wemirr/workflow/process/workflow.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <PageWrapper> hello workflow </PageWrapper>
-</template>
-<script lang="ts" setup>
-import { PageWrapper } from "@/components/Page";
-</script>
