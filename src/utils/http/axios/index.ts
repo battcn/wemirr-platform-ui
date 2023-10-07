6381ba1ee391e254cfb447248db01f30a33246cd
唐亚峰 <battcn@cortp.com>
2023-10-07 14:37:43 +0800

    feat：调整超时

--- src/utils/http/axios/index.ts
+++ src/utils/http/axios/index.ts
@@ -230,7 +230,7 @@ function createAxios(opt?: Partial<CreateAxiosOptions>) {
         // authentication schemes，e.g: Bearer
         authenticationScheme: "Bearer",
         // authenticationScheme: '',
-        timeout: 20 * 1000,
+        timeout: 30 * 1000,
         // 基础接口地址
         // baseURL: globSetting.apiUrl,
 
