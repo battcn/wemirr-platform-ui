f3f03b63eba5bf5164300643ab3def19c756ca5a
唐亚峰 <battcn@cortp.com>
2023-10-10 09:40:38 +0800

    feat：vben fixbug代码同步（Commits on Oct 9, 2023）

--- src/components/Table/src/components/editable/EditableCell.vue
+++ src/components/Table/src/components/editable/EditableCell.vue
@@ -70,7 +70,7 @@ export default defineComponent({
       const valueField = isCheckValue ? "checked" : "value";
       const val = unref(currentValueRef);
 
-      const value = isCheckValue ? (isNumber(val) && isBoolean(val) ? val : !!val) : val;
+      const value = isCheckValue ? (isNumber(val) || isBoolean(val) ? val : !!val) : val;
 
       let compProps = props.column?.editComponentProps ?? ({} as any);
       const { record, column, index } = props;
