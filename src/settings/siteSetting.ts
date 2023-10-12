6b7e993498319bafe98e5818892b328cf0665c41
唐亚峰 <battcn@cortp.com>
2023-10-12 17:20:21 +0800

    feat：调整文档地址

--- src/settings/siteSetting.ts
+++ src/settings/siteSetting.ts
@@ -2,7 +2,7 @@
 export const GITHUB_URL = "https://github.com/battcn/wemirr-platform-ui";
 
 // vue-skill-full-next-doc
-export const DOC_URL = "https://github.com/battcn/wemirr-platform-ui";
+export const DOC_URL = "https://docs.battcn.com";
 
 // site url
-export const SITE_URL = "https://cloud.battcn.com";
+export const SITE_URL = "https://docs.battcn.com";
