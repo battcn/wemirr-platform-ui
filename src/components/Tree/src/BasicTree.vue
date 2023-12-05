14b83ab1817af2224c088457473dbdf48b1f9886
唐亚峰 | battcn <1837307557@qq.com>
2023-12-05 08:53:39 +0800

    feat：vben fixbug代码同步（Commits on Dec 4, 2023）

--- src/components/Tree/src/BasicTree.vue
+++ src/components/Tree/src/BasicTree.vue
@@ -110,12 +110,12 @@ export default defineComponent({
       return omit(propsData, "treeData", "class") as TreeProps;
     });
 
-    const getTreeData = computed((): TreeItem[] =>
+    const getTreeSearchData = computed((): TreeItem[] =>
       searchState.startSearch ? searchState.searchData : unref(treeDataRef),
     );
 
     const getNotFound = computed((): boolean => {
-      return !getTreeData.value || getTreeData.value.length === 0;
+      return !getTreeSearchData.value || getTreeSearchData.value.length === 0;
     });
 
     const {
@@ -156,6 +156,10 @@ export default defineComponent({
       if (!contextMenuOptions.items?.length) return;
       contextMenuOptions.items = contextMenuOptions.items.filter((item) => !item.hidden);
       createContextMenu(contextMenuOptions);
+    }
+
+    function getTreeData() {
+      return unref(treeDataRef);
     }
 
     function setExpandedKeys(keys: KeyType[]) {
@@ -319,7 +323,6 @@ export default defineComponent({
     });
 
     const instance: TreeActionType = {
-      getTreeData: () => treeDataRef,
       setExpandedKeys,
       getExpandedKeys,
       setSelectedKeys,
@@ -366,7 +369,7 @@ export default defineComponent({
     }
 
     const treeData = computed(() => {
-      const data = cloneDeep(getTreeData.value);
+      const data = cloneDeep(getTreeSearchData.value);
       eachTree(data, (item, _parent) => {
         const searchText = searchState.searchText;
         const { highlight } = unref(props);
