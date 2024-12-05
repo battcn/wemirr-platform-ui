84368c8a6c96f8b040aaef60ff5fa201333f835f
唐亚峰 | battcn <1837307557@qq.com>
2024-12-05 23:51:18 +0800

    feat: 消息推送

--- apps/web-antd/src/views/wemirr/system/message/publish.ts
+++ apps/web-antd/src/views/wemirr/system/message/publish.ts
@@ -1,119 +1,127 @@
-import {compute, dict, useColumns, useUi} from "@fast-crud/fast-crud";
-import createCrudOptionsText from "../user/crud";
-import {defHttp} from "#/api/request";
+import { compute, dict, useColumns, useUi } from '@fast-crud/fast-crud';
+
+import { defHttp } from '#/api/request';
+
+import createCrudOptionsText from '../user/crud';
 
 export function createFormOptions() {
-    const {ui} = useUi();
-    // 自定义表单配置
-    const {buildFormOptions} = useColumns();
-    //使用crudOptions结构来构建自定义表单配置
-    return buildFormOptions({
-        columns: {
-            code: {
-                title: "编码",
-                type: "text",
-                form: {
-                    component: {disabled: true},
-                },
+  const { ui } = useUi();
+  // 自定义表单配置
+  const { buildFormOptions } = useColumns();
+  // 使用crudOptions结构来构建自定义表单配置
+  return buildFormOptions({
+    columns: {
+      templateId: {
+        title: '编码',
+        type: 'text',
+        form: {
+          show: false,
+        },
+      },
+      code: {
+        title: '编码',
+        type: 'text',
+        form: {
+          component: { disabled: true },
+        },
+      },
+      name: {
+        title: '名称',
+        type: 'text',
+        form: {
+          component: { disabled: true },
+        },
+      },
+      content: {
+        title: '消息内容',
+        type: 'textarea',
+        form: {
+          col: { span: 24 },
+          component: { disabled: true },
+        },
+      },
+      subscriberType: {
+        title: '类型',
+        type: 'dict-select',
+        dict: dict({
+          data: [
+            { value: 0, label: '用户', color: 'success' },
+            // { value: 1, label: "角色", color: "error" },
+          ],
+        }),
+        form: {
+          value: 0,
+          rules: [{ required: true, message: '订阅类型不能为空' }],
+        },
+      },
+      subscriberIdList: {
+        title: '订阅人',
+        type: 'table-select',
+        dict: dict({
+          value: 'id',
+          label: 'nickName',
+          // 重要，根据value懒加载数据
+          getNodesByValues: async (values: any[]) => {
+            return defHttp.post('/iam/users/ids', values);
+          },
+        }),
+        form: {
+          rules: [{ required: true, message: '订阅人不能为空' }],
+          show: compute(({ form }) => {
+            return form.subscriberType === 0;
+          }),
+          component: {
+            crossPage: true,
+            multiple: true,
+            valuesFormat: {
+              labelFormatter: (item: any) => {
+                return `${item.nickName}`;
+              },
             },
-            name: {
-                title: "名称",
-                type: "text",
-                form: {
-                    component: {disabled: true},
-                },
+            select: {
+              placeholder: '点击选择',
             },
-            content: {
-                title: "消息内容",
-                type: "textarea",
-                form: {
-                    col: {span: 24},
-                    component: {disabled: true},
-                },
+            createCrudOptions: createCrudOptionsText,
+            crudOptionsOverride: {
+              toolbar: { show: false },
+              actionbar: { buttons: { add: { show: false } } },
+              rowHandle: {
+                show: false,
+              },
+              columns: {
+                username: { column: { show: false } },
+                status: { column: { show: false } },
+                nation: { column: { show: false } },
+                education: { column: { show: false } },
+                positionId: { column: { show: false } },
+              },
             },
-            type: {
-                title: "类型",
-                type: "dict-select",
-                dict: dict({
-                    data: [
-                        {value: 0, label: "用户", color: "success"},
-                        // { value: 1, label: "角色", color: "error" },
-                    ],
-                }),
-                form: {
-                    value: 0,
-                    rules: [{required: true, message: "订阅类型不能为空"}],
-                }
+            on: {
+              selectedChange({ $event }) {
+                ui.notification.success(`你选择了${JSON.stringify($event)}`);
+              },
             },
-            subscriberIdList: {
-                title: "订阅人",
-                type: "table-select",
-                dict: dict({
-                    value: "id",
-                    label: "nickName",
-                    //重要，根据value懒加载数据
-                    getNodesByValues: async (values: any[]) => {
-                        return defHttp.post('/iam/users/ids', values);
-                    }
-                }),
-                form: {
-                    rules: [{required: true, message: "订阅人不能为空"}],
-                    show: compute(({form}) => {
-                        return form.type === 0;
-                    }),
-                    component: {
-                        crossPage: true,
-                        multiple: true,
-                        valuesFormat: {
-                            labelFormatter: (item: any) => {
-                                return `${item.nickName}`;
-                            }
-                        },
-                        select: {
-                            placeholder: "点击选择"
-                        },
-                        createCrudOptions: createCrudOptionsText,
-                        crudOptionsOverride: {
-                            toolbar: {show: false},
-                            actionbar: {buttons: {add: {show: false}}},
-                            rowHandle: {
-                                show: false,
-                            },
-                            columns: {
-                                username: {column: {show: false}},
-                                status: {column: {show: false}},
-                                nation: {column: {show: false}},
-                                education: {column: {show: false}},
-                                positionId: {column: {show: false}},
-                            },
-                        },
-                        on: {
-                            selectedChange({$event}) {
-                                console.log("selectedChange", $event);
-                                // ui.notification.success(`你选择了${JSON.stringify($event)}`)
-                            }
-                        }
-                    }
-                }
-            },
-            variables: {
-                title: "变量",
-                type: ["text"],
-                column: {
-                    show: false,
-                    component: {name: "fs-values-format"},
-                },
-                form: {
-                    col: {span: 24},
-                },
-            },
+          },
+        },
+      },
+      variables: {
+        title: '变量',
+        type: ['text'],
+        column: {
+          show: false,
+          component: { name: 'fs-values-format' },
         },
         form: {
-            doSubmit({form}) {
-                defHttp.post("/iam/message-template/notify", form).then(() => {
-                    ui.notification.success("消息推送成功")
-                })
-            }
-        }
-    });
+          col: { span: 24 },
+        },
+      },
+    },
+    form: {
+      doSubmit({ form }) {
+        defHttp.post('/iam/message-notify/publish', form).then(() => {
+          ui.notification.success('消息推送成功');
+        });
+      },
+    },
+  });
 }
--- apps/web-antd/src/views/wemirr/system/message/publish.vue
+++ apps/web-antd/src/views/wemirr/system/message/publish.vue
@@ -1,43 +1,14 @@
-<template>
-  <Page
-      content-class="w-height flex flex-col gap-4"
-      description="提供给运营人员操作消息推送"
-      title="消息推送"
-  >
-    <Card>
-      <fs-form ref="formRef" v-bind="formOptions">
-        <template #form_variables="scope">
-          <div v-for="(val, key) in scope.form.variables" :key="key">
-<!--            {{ key }} -  {{ val }}-->
-            <a-form-item>
-              <a-input
-                  :value="key"
-                  placeholder="参数键"
-                  style="width: 23%; margin-right: 8px"
-              />
-              <a-input
-                  :value="val"
-                  placeholder="参数值"
-                  style="width: 22%; margin-right: 8px"
-              />
-            </a-form-item>
-          </div>
-        </template>
-      </fs-form>
-      <div style="margin-top: 50px;float: right">
-        <a-button v-if="formRef" type="primary" @click="formRef.submit">发布</a-button>
-      </div>
-    </Card>
-  </Page>
-</template>
-
 <script lang="ts" setup>
-import {useRoute} from "vue-router";
-import {Card} from 'ant-design-vue';
-import {Page} from "@vben/common-ui";
-import {createFormOptions} from "./publish";
-import {defHttp} from "#/api/request";
-import {onMounted, ref} from "vue";
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+import { Page } from '@vben/common-ui';
+
+import { Card } from 'ant-design-vue';
+
+import { defHttp } from '#/api/request';
+
+import { createFormOptions } from './publish';
 
 const formRef = ref();
 const formOptions = ref();
@@ -45,18 +16,51 @@ formOptions.value = createFormOptions();
 const route = useRoute();
 const id: any = route.query.id;
 
-onMounted(async() => {
-  // let variables = {'username': 'test', email: 'abc@63.com'}
-  // formOptions.value.initialForm = {code: 'xxxx', name: 'name', context: '欢迎 {username} 来到 WP 系统', variables};
+onMounted(async () => {
   defHttp.get(`/iam/message-template/${id}/detail`).then((ret) => {
-    let entries = ret.variables?.map((item: any) => [item, '1']);
-    console.log(Object.fromEntries(entries))
+    const entries = ret.variables?.map((item: any) => [item, '1']);
     formRef.value.setFormData({
+      templateId: ret.id,
       code: ret.code,
       name: ret.name,
       content: ret.content,
       variables: Object.fromEntries(entries),
-    })
-  })
-})
+    });
+  });
+});
 </script>
+
+<template>
+  <Page
+    content-class="w-height flex flex-col gap-4"
+    description="提供给运营人员操作消息推送"
+    title="消息推送"
+  >
+    <Card>
+      <fs-form ref="formRef" v-bind="formOptions">
+        <template #form_variables="scope">
+          <div v-for="(val, key) in scope.form.variables" :key="key">
+            <!--            {{ key }} -  {{ val }}-->
+            <a-form-item>
+              <a-input
+                :value="key"
+                placeholder="参数键"
+                style="width: 23%; margin-right: 8px"
+              />
+              <a-input
+                :value="val"
+                placeholder="参数值"
+                style="width: 22%; margin-right: 8px"
+              />
+            </a-form-item>
+          </div>
+        </template>
+      </fs-form>
+      <div style="float: right; margin-top: 50px">
+        <a-button v-if="formRef" type="primary" @click="formRef.submit">
+          发布
+        </a-button>
+      </div>
+    </Card>
+  </Page>
+</template>
