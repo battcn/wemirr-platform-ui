664078d4127e94922710b0a3ab06bc8510d6be14
唐亚峰 | battcn <1837307557@qq.com>
2025-07-26 14:08:59 +0800

    feat:同步 vben

--- apps/web-antd/src/router/routes/core.ts
+++ apps/web-antd/src/router/routes/core.ts
@@ -3,7 +3,6 @@ import type { RouteRecordRaw } from 'vue-router';
 import { LOGIN_PATH } from '@vben/constants';
 import { preferences } from '@vben/preferences';
 
-import { AuthPageLayout } from '#/layouts';
 import { $t } from '#/locales';
 
 const BasicLayout = () => import('#/layouts/basic.vue');
