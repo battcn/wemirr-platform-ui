adfccabffa0838843cc28057052eb32ca4feb103
唐亚峰 | battcn <1837307557@qq.com>
2025-12-04 22:27:28 +0800

    feat: 调整 epic-designer

--- apps/web-antd/src/api/request.ts
+++ apps/web-antd/src/api/request.ts
@@ -14,7 +14,7 @@ import {
 import { useAccessStore } from '@vben/stores';
 
 import { useUi } from '@fast-crud/fast-crud';
-import { nanoid } from 'nanoid';
+// import { nanoid } from 'nanoid';
 
 import { useAuthStore } from '#/store';
 
@@ -67,7 +67,7 @@ function createRequestClient(baseURL: string, options?: RequestClientOptions) {
       const accessStore = useAccessStore();
 
       config.headers.Authorization = formatToken(accessStore.accessToken);
-      config.headers['x-request-id'] = nanoid();
+      // config.headers['x-request-id'] = nanoid();
       config.headers['Accept-Language'] = preferences.app.locale;
       return config;
     },
